7a3c230ce6bc2fadf2a9b5fd4a11df8d50610f0c
juan2three <sk8erjuan@gmail.com>
2017-08-21 13:58:14 -0400

    new ownership
    new ownership

---
+++ js/javascript.js
@@ -0,0 +1,73 @@
+// wait until page is ready to perform javascript
+
+$(document).ready(function() {
+
+    $(".table-date .day" + (new Date().getDay())).addClass("today-date-row");
+
+	//setup current testimonial index and testimonial data array's
+	var currentTest = 0;
+    var testimonials = ["Took my dog here for the first time for a Skin issue and the vet knew exactly what was wrong and how to treat the issue. The staff were friendly and professional. The prices for services were also reasonable.", 
+                        "I come all the way from Orlando.  Best vet I've been to.  Recommended to all my friends.", 
+                        "Dr. Wayne and staff are great. Our three dogs get super care. Very polite and caring."];
+    var testimonialNames = ["Tracy W.","Christopher B.","Ray Mikler"];
+    var testimonialImages = [ "/images/testimonials/test1Image.jpg","/images/testimonials/test2Image.jpg","/images/testimonials/test3Image.jpg"];
+
+    // initially set the content of the testimonial elements to index 0
+    $('.testimonialText').text(testimonials[currentTest]);
+    $('.testimonialName').text(testimonialNames[currentTest]);
+    $('.testimonialImage').attr("src",testimonialImages[currentTest]);
+
+    setInterval(function() {testSwap()}, 10000);
+
+    // function for when user clicks the right arrow
+    $('#rightButton').click(function(){
+
+    	// we only have three testimonials so if user clicks right at the last index reset
+    	// index to 0 to loop through the array again
+    	if (currentTest == (testimonials.length - 1) ) {
+    		currentTest = 0;
+    	}
+    	else {
+    		currentTest++;
+    	}
+    	
+    	// display changed testimonials
+    	$('.testimonialText').text(testimonials[currentTest]);
+    	$('.testimonialName').text(testimonialNames[currentTest]);
+        $( '.testimonialImage' ).attr("src",testimonialImages[currentTest]);
+    });
+
+    // function for when user clicks the left arrow
+    $('#leftButton').click(function(){
+
+    	// if user clicks left arrow when we are on first index start over from last index
+    	if ( currentTest == 0 ) {
+    		currentTest = testimonials.length - 1;
+    	}
+    	else {
+    		currentTest--;
+    	}
+    	
+    	// display changed testimonials
+    	$('.testimonialText').text(testimonials[currentTest]);
+    	$('.testimonialName').text(testimonialNames[currentTest]);
+        $('.testimonialImage').attr("src",testimonialImages[currentTest]);
+    });
+
+    function testSwap() {
+        if ( currentTest != 2 ) {
+            currentTest++;
+            $( '.testimonialText').text(testimonials[currentTest]);
+            $( '.testimonialName' ).text(testimonialNames[currentTest]);
+            $( '.testimonialImage' ).attr("src",testimonialImages[currentTest]);
+        }
+        else {
+            currentTest = 0;
+            $( '.testimonialText').text(testimonials[currentTest]);
+            $( '.testimonialName' ).text(testimonialNames[currentTest]);
+            $( '.testimonialImage' ).attr("src",testimonialImages[currentTest]);
+        }
+    }
+});
+
+cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () { alert("Made with <3 by the 2015 HDSupply Interns"); });
---
+++ js/modal.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+
+if (document.location.pathname.indexOf("cats") > -1) {
+    loadPetImages('/js/cats.json','cats');
+} 
+else if (document.location.pathname.indexOf("dogs") > -1) {
+    loadPetImages('/js/dogs.json','dogs');
+} 
+else {
+    console.error('unable to load images');
+}
+
+});
+
+function loadPetImages(url, petType) {
+    $.getJSON(url, function(json) {
+    var attributes = new Array();
+    var imageGallery = $('.image-gallery');
+    var imageGalleryThumbnail = $('.image-gallery .thumbnail');
+
+    if (json[petType].length > 0) {
+        imageGallery.find('.no-pets').addClass('hidden');
+        imageGallery.find('.pets-available').removeClass('hidden');
+    }
+    else {
+        imageGallery.find('.no-pets').removeClass('hidden');
+        imageGallery.find('.pets-available').addClass('hidden');
+    }
+ 
+    for (var i=0; i<json[petType].length; i++) {
+        var pet = json[petType][i];
+        var imgUrl = pet.image;
+        var thumbnail = imageGalleryThumbnail.clone();
+        thumbnail.removeClass('hidden-lg');
+        thumbnail.removeClass('hidden-md');
+        thumbnail.removeClass('hidden-sm');
+        thumbnail.removeClass('hidden-xs');
+        thumbnail.data('thumb',i);
+        thumbnail.find('ul li').text(pet.name);
+        thumbnail.find('img').attr('src', imgUrl);
+        imageGallery.append(thumbnail);
+        attributes.push({img: imgUrl, name: pet.name, age: pet.age, gender: pet.gender, breed: pet.breed, color: pet.color, behavior: pet.behavior, needs: pet.needs}); 
+    }
+    
+    function setModalContent($thumbNumber) {
+        $('#myModal .modal-body img').last().attr('src',attributes[$thumbNumber].img);
+        $('#myModal .name').html('<b>Name:</b> ' + attributes[$thumbNumber].name);
+        $('#myModal .age').html('<b>Age:</b> ' + attributes[$thumbNumber].age);
+        $('#myModal .gender').html('<b>Gender:</b> ' + attributes[$thumbNumber].gender);
+        $('#myModal .breed').html('<b>Breed:</b> ' + attributes[$thumbNumber].breed);
+        $('#myModal .color').html('<b>Color:</b> ' + attributes[$thumbNumber].color);
+        $('#myModal .behavior').html('<b>Behaviors:</b> ' + attributes[$thumbNumber].behavior);
+        if (attributes[$thumbNumber].needs.length != 0) {
+            $('#myModal .needs').html('<b>Special Needs:</b> ' + attributes[$thumbNumber].needs).removeClass('hidden');
+        } 
+        else {
+            $('#myModal .needs').addClass('hidden');
+        }
+        $('#myModal .pet-name').html('Want to take ' + attributes[$thumbNumber].name + ' home?');
+
+
+        /* set next and previous buttons */
+        $('.prev-img').data('thumb',($thumbNumber - 1 >= 0) ? $thumbNumber - 1 : attributes.length - 1);
+        $('.next-img').data('thumb',($thumbNumber + 1 < attributes.length) ? $thumbNumber + 1 : 0);
+    }   
+
+    $('.openmodal').on('click',function(){
+        setModalContent($( this ).data('thumb'));
+        $('#myModal').modal('show');//pop up modal
+    });
+
+    $('.modal').on('click','.prev-img',function(){ setModalContent($( this ).data('thumb'));});
+    $('.modal').on('click','.next-img',function(){ setModalContent($( this ).data('thumb'));});
+
+    });
+}
+
+
